bd80bbcc4c99f1332dc6b900f00678cd3f214197
ChaitanyaPattem <pchaitu1996@gmail.com>
2021-10-07 17:49:35 +0530

    first commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
+
+
+export default function product(props) {
+    const { product } = props
+    return (
+        <div className="card" key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <img class="medium" src={product.image} alt={product.name} />
+            </Link>
+            <div className="card-body">
+              <Link to={`/product/${product._id}`}>
+                <h2>{product.name}</h2>
+              </Link>
+              <Rating rating={product.rating} numReviews={product.numReviews}/>
+                <div class="price">${product.price}</div>
+            </div>
+        </div>
+    )
+}
